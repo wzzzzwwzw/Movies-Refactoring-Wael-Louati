df2306259d2f1a6e0e5a8a06b4045161d8439a27
wzzzzwwzw <46055260+wzzzzwwzw@users.noreply.github.com>
2022-04-10 15:38:10 -0800

    [ Collapse Class Price ] Delete NewReleasePrice Constants

--- src/main/java/usantatecla/movies/v21/NewReleasePrice.java
+++ src/main/java/usantatecla/movies/v21/NewReleasePrice.java
@@ -2,9 +2,6 @@ package usantatecla.movies.v21;
 
 public class NewReleasePrice extends Price {
 
-	private static final double CHARGE = 3;
-	private static final int DAYS_RENTED_THRESHOLD = 1;
-	private static final int FREQUENT_RENTER_POINTS = 2;
 	
 	@Override
 	public double getCharge(int daysRented) {
