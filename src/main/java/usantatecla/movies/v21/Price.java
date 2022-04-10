b4931df838712c7e37137d35455ef148f5247e94
wzzzzwwzw <46055260+wzzzzwwzw@users.noreply.github.com>
2022-04-10 15:28:02 -0800

    [ Collapse Class Price ] Add Price details constants

--- src/main/java/usantatecla/movies/v21/Price.java
+++ src/main/java/usantatecla/movies/v21/Price.java
@@ -2,6 +2,12 @@ package usantatecla.movies.v21;
 
 public abstract class Price {
 
+	private static final double CHARGE = 2.0;
+
+	private static final double EXTRA_CHARGE = 1.5;
+
+	private static final int DAYS_RENTED_THRESHOLD = 2;
+
 	private static final int FREQUENT_RENTER_POINTS = 1;
 	
 	public abstract double getCharge(int daysRented);
