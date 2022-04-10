271928dfb41504485675323218b3e9ddc50628c7
wzzzzwwzw <46055260+wzzzzwwzw@users.noreply.github.com>
2022-04-10 15:29:51 -0800

    [ Collapse Class Price ] Replace Private constants to Protected

--- src/main/java/usantatecla/movies/v21/Price.java
+++ src/main/java/usantatecla/movies/v21/Price.java
@@ -2,11 +2,11 @@ package usantatecla.movies.v21;
 
 public abstract class Price {
 
-	private static final double CHARGE = 2.0;
+	protected static final double CHARGE = 2.0;
 
-	private static final double EXTRA_CHARGE = 1.5;
+	protected static final double EXTRA_CHARGE = 1.5;
 
-	private static final int DAYS_RENTED_THRESHOLD = 2;
+	protected static final int DAYS_RENTED_THRESHOLD = 2;
 
 	private static final int FREQUENT_RENTER_POINTS = 1;
 	
