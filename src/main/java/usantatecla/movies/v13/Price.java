b046f411baac73599a78dc02323490609bbff3a1
wzzzzwwzw <46055260+wzzzzwwzw@users.noreply.github.com>
2022-04-10 15:11:22 -0800

    Removing Packages

--- src/main/java/usantatecla/movies/v13/Price.java
+++
@@ -1,6 +0,0 @@
-package usantatecla.movies.v13;
-
-public abstract class Price {
-
-	abstract int getPriceCode();
-}
--- src/main/java/usantatecla/movies/v14/Price.java
+++
@@ -1,6 +0,0 @@
-package usantatecla.movies.v14;
-
-public abstract class Price {
-
-	abstract int getPriceCode();
-}
