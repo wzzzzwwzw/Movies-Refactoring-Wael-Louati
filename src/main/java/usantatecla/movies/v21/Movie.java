ff03db705c749c8e86c1a5a7ada64ec005c8ac95
wzzzzwwzw <46055260+wzzzzwwzw@users.noreply.github.com>
2022-04-10 16:21:53 -0800

    [ Collapse Class Price ] Create,Replace,Delete Price Class Constants

--- src/main/java/usantatecla/movies/v21/Movie.java
+++ src/main/java/usantatecla/movies/v21/Movie.java
@@ -2,6 +2,27 @@ package usantatecla.movies.v21;
 
 public class Movie {
 
+	protected static final double CHARGE_REGULAR = 2.0;
+
+	protected static final double EXTRA_CHARGE_REGULAR = 1.5;
+
+	protected static final int DAYS_RENTED_THRESHOLD_REGULAR = 2;
+
+	protected static final double CHARGE_NEW_RELEASE = 3;
+
+	protected static final int DAYS_RENTED_THRESHOLD_NEW_RELEASE = 1;
+
+	protected static final int FREQUENT_RENTER_POINTS_NEW_RELEASE = 2;
+
+	protected static final double CHARGE_CHILDREN = 1.5;
+
+	protected static final double EXTRA_CHARGE_CHILDREN = 1.5;
+
+	protected static final int DAYS_RENTED_THRESHOLD_CHILDREN = 3;
+
+	static final int FREQUENT_RENTER_POINTS = 1;
+
+
 	private Price price;
 
 	private String title;
@@ -34,7 +55,7 @@ public class Movie {
 			return price.getFrequentRenterPointsNewReleases(daysRented);
 		}
 		else {
-			return price.getFrequentRenterPoints(daysRented);
+			return (int) price.getFrequentRenterPoints(daysRented);
 		}
 	}
 	
--- src/main/java/usantatecla/movies/v21/Price.java
+++ src/main/java/usantatecla/movies/v21/Price.java
@@ -2,59 +2,48 @@ package usantatecla.movies.v21;
 
 public  class Price {
 
-	protected static final double CHARGE_REGULAR = 2.0;
 
-	protected static final double EXTRA_CHARGE_REGULAR = 1.5;
-
-	protected static final int DAYS_RENTED_THRESHOLD_REGULAR = 2;
-
-    protected static final double CHARGE_NEW_RELEASE = 3;
-
-	protected static final int DAYS_RENTED_THRESHOLD_NEW_RELEASE = 1;
-
-	protected static final int FREQUENT_RENTER_POINTS_NEW_RELEASE = 2;
-
-	private static final int FREQUENT_RENTER_POINTS = 1;
-
-	protected static final double CHARGE_CHILDREN = 1.5;
-
-	protected static final double EXTRA_CHARGE_CHILDREN = 1.5;
-
-	protected static final int DAYS_RENTED_THRESHOLD_CHILDREN = 3;
 
 	public double getCharge(int daysRented) {
 		return 0;
 	}
 
 	public double getChargeRegular(int daysRented) {
-		double result = Price.CHARGE_REGULAR;
-		if (daysRented > Price.DAYS_RENTED_THRESHOLD_REGULAR) {
-			result += (daysRented - Price.DAYS_RENTED_THRESHOLD_REGULAR) * Price.EXTRA_CHARGE_REGULAR;
+		double result = Movie.CHARGE_REGULAR;
+		if (daysRented > Movie.DAYS_RENTED_THRESHOLD_REGULAR) {
+			result += (daysRented - Movie.DAYS_RENTED_THRESHOLD_REGULAR) * Movie.EXTRA_CHARGE_REGULAR;
 		}
 		return result;
+
 	}
+
 
 	public double getChargeNewReleases(int daysRented) {
-		return Price.CHARGE_NEW_RELEASE;
-	}
-
-	public int getFrequentRenterPointsNewReleases(int daysRented) {
-		if (daysRented > Price.DAYS_RENTED_THRESHOLD_NEW_RELEASE) {
-			return Price.FREQUENT_RENTER_POINTS_NEW_RELEASE;
-		} else {
-			return this.getFrequentRenterPoints(daysRented);
-		}
+		return Movie.CHARGE_NEW_RELEASE;
 	}
 
 	public double getChargeChildren(int daysRented) {
-		double result = Price.CHARGE_CHILDREN;
-		if (daysRented > Price.DAYS_RENTED_THRESHOLD_CHILDREN) {
-			result += (daysRented - 1) * Price.EXTRA_CHARGE_CHILDREN;
+		double result = Movie.CHARGE_CHILDREN;
+		if (daysRented > Movie.DAYS_RENTED_THRESHOLD_CHILDREN) {
+			result += (daysRented - 1) * Movie.EXTRA_CHARGE_CHILDREN;
 		}
 		return result;
 	}
-	
-	public int getFrequentRenterPoints(int daysRented) {
-		return Price.FREQUENT_RENTER_POINTS;
+
+	public int getFrequentRenterPointsNewReleases(int daysRented) {
+
+		if (daysRented > Movie.DAYS_RENTED_THRESHOLD_NEW_RELEASE) {
+			return Movie.FREQUENT_RENTER_POINTS_NEW_RELEASE;
+		} else {
+			return (int) this.getFrequentRenterPoints(daysRented);
+		}
 	}
+
+
+	double getFrequentRenterPoints(int daysRented) {
+		return Movie.FREQUENT_RENTER_POINTS;
+	}
+
 }
+
+
